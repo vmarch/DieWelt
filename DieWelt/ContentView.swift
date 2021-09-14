9327d8b729e03e182dbf7b7011fb741cc769fb93
vmarch <marchenkoukr@gmail.com>
2021-09-14 15:25:48 +0200

    Initial Commit

---
+++ DieWelt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DieWelt
+//
+//  Created by Student on 14.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DieWelt/DieWeltApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieWeltApp.swift
+//  DieWelt
+//
+//  Created by Student on 14.09.21.
+//
+
+import SwiftUI
+
+@main
+struct DieWeltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
